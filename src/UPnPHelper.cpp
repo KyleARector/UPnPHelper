d32824ba3b1057c22cce6cadc5b09872258441a4
Kyle Rector <kylearector@outlook.com>
2017-08-31 11:02:39 -0400

    Moving definition of response message to main Arduino sketch

--- src/UPnPHelper.cpp
+++ src/UPnPHelper.cpp
@@ -1,7 +1,22 @@
+/////////////////////////////////////////////////////
+//                   UPnPHelper.cpp                //
+/////////////////////////////////////////////////////
+//                       Notes                     //
+// Provides resources for UPnP message             //
+// and receiving on an Arduino using the WiFi      //
+// libraries. Specifically, responds to M-Search   //
+// packets with a message determined in the        //
+// main Arduino sketch.                            //
+//                                                 //
+// Would like to remove dependency on "String"     //
+/////////////////////////////////////////////////////
+//                     License                     //
+// GNU Public License 3.0                          //
+/////////////////////////////////////////////////////
 #include "UPnPHelper.h"
 
 // UPnP multicast address
-IPAddress addr(239, 255, 255, 250);
+const IPAddress addr(239, 255, 255, 250);
 // UPnP Port
 const unsigned int port = 1900;
 // Buffer for received packets
@@ -14,7 +29,9 @@ UPnPHelper::UPnPHelper() { }
 UPnPHelper::~UPnPHelper() { }
 
 // Starts the UDP server
-bool UPnPHelper::Start() {
+// Accepts the response packet to be sent to the searching device
+bool UPnPHelper::Start(const String &response) {
+  response_packet = response;
   // Attempt to multicast on the uPnP channel
   if(UDP.beginMulticast(WiFi.localIP(), addr, port)) {
     // If successful return true
@@ -40,7 +57,22 @@ bool UPnPHelper::Read() {
   return false;
 }
 
+// Await an M-SEARCH packet, and respond if search meets criteria
+void UPnPHelper::AwaitSearch() {
+  if(Read()) {
+    // Put packet contents in string
+    String packet_content = String((char *)in_packet);
+    // If the packet is an M-SEARCH packet, respond
+    if(packet_content.indexOf('M-SEARCH') > 0) {
+      Respond();
+    }
+  }
+}
+
 // Send a packet to the address the last request came from
 void UPnPHelper::Respond() {
-
+  UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
+  // Send the String's underlying byte array
+  UDP.write(response_packet.c_str());
+  UDP.endPacket();
 }
--- src/UPnPHelper.h
+++ src/UPnPHelper.h
@@ -1,3 +1,11 @@
+/////////////////////////////////////////////////////
+//                    UPnPHelper.h                 //
+/////////////////////////////////////////////////////
+//                       Notes                     //
+/////////////////////////////////////////////////////
+//                     License                     //
+// GNU Public License 3.0                          //
+/////////////////////////////////////////////////////
 #ifndef UPNPHELPER_H_
 #define UPNPHELPER_H_
 
@@ -5,16 +13,21 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
+// Provides an interface to UDP resources and methods to respond to UPnP
+// searches and requests
 class UPnPHelper {
   private:
     // UDP connection for multicast and response to SSDP
     WiFiUDP UDP;
+    // The response to be sent out after M-SEARCH requests
+    String response_packet;
 
   public:
     UPnPHelper();
     ~UPnPHelper();
-    bool Start();
+    bool Start(const String &response);
     bool Read();
+    void AwaitSearch();
     void Respond();
 };
 
